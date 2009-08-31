f2723f4bc59ddff2e974243d5a69dfdb34e7222b
jlcanela <jlcanela@hexabuntu.(none)>
2009-08-31 23:39:23 +0200

    cleaning boot dir

--- test/testSchema1.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE attributes (id INTEGER PRIMARY KEY, name TEXT, min NUMERIC, max NUMERIC, category INTEGER);
-CREATE TABLE database_version (version_number INTEGER);
-CREATE TABLE data (id INTEGER PRIMARY KEY, letter TEXT );
-CREATE TABLE events (start_day INTEGER, duration INTEGER );
-CREATE INDEX idx_index ON attributes (don't care);
